c82e435a9691821fb33accb6141a8b383d6f42bc
Carlos Castillo <carloscacua@gmail.com>
2017-10-16 21:36:17 -0400

    add missing file

--- src/main/java/WS/HeaderController.java
+++ src/main/java/WS/HeaderController.java
@@ -4,10 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/header")
 public class HeaderController {
-
-
-    @RequestMapping(value = "/header", method = RequestMethod.GET)
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String CheckHeader() {
         return "Success";
     }
