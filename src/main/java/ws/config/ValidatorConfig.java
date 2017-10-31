201a9f2337fa25d977ec695bde9915a27446f8a9
Carlos Castillo <carloscacua@gmail.com>
2017-10-30 21:27:56 -0400

    and header validation only for header controller in order to allow docs access

--- src/main/java/ws/config/ValidatorConfig.java
+++ src/main/java/ws/config/ValidatorConfig.java
@@ -14,6 +14,6 @@ public class ValidatorConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(headerValidator);
+        registry.addInterceptor(headerValidator).addPathPatterns("/header/**");
     }
 }
